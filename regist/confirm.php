0a622d70b2f9e528ff08a3a12365e2050a98e7e6
XIEXIE143 <xjy143@outlook.com>
2023-10-23 09:40:11 +0900

    update

---
+++ regist/confirm.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Tweet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <main id="id" class="d-flex justify-content-center">
+        <div class="w-50 mt-3 p-5 bg-light">
+            <h2 class="h2 mb-3 fw-normal text-center">Regist</h2>
+            <p>この内容で登録しますか？</p>
+            <form action="" method="post">
+                <div class="mb-2">
+                    <label for="" class="form-label">Name</label>
+                    xxxx
+                </div>
+                <div class="mb-2">
+                    <label for="" class="form-label">Email</label>
+                    xxxx
+                </div>
+                <div>
+                    <button class="w-100 mb-2 btn btn-primary">Regist</button>
+                    <a href="input.php" class="w-100 btn btn-outline-primary">Back</a>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
