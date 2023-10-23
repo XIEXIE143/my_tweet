c2d3cb1c7d50f05e426dea490eefec8fe3922e9a
XIEXIE143 <xjy143@outlook.com>
2023-10-23 11:14:52 +0900

    up

--- regist/add.php
+++ regist/add.php
@@ -3,6 +3,10 @@
 session_start();
 session_regenerate_id(true);
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit('can not get access');
+}
+
 $post = $_SESSION['regist'];
 
 header('Location: result.php');
